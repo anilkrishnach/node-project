e08d628040fc766f9fea26aa2367e3a9fb3c7429
Anil <anilkrishna11@gmail.com>
2018-02-25 00:46:01 +0530

    Set up start and heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs=require('hbs');
 const fs=require('fs');
+const port = process.env.PORT||3000;
 var app = express();
 hbs.registerPartials(__dirname+'/views/partials');
 hbs.registerHelper('upper',(test)=>{
@@ -33,5 +34,5 @@ app.post('/homepage',(req,res)=>{
   console.log(req.body.rno);
   console.log(req.body.pwd);
 });
-app.listen(3000, ()=>{
-console.log("Server is up!");});
+app.listen(port, ()=>{
+console.log("Server is up at "+port);});
